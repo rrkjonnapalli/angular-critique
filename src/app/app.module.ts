12a843824bda8d08660261ca87e138a75242179a
rrkjonnapalli <r.ravikiranjonnapalli@gmail.com>
2019-06-10 17:31:43 +0530

    skeleton created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,17 +9,18 @@ import { FootComponent } from './components/foot/foot.component';
 import { MainComponent } from './components/main/main.component';
 import { EditorComponent } from './components/parts/editor/editor.component';
 import { ViewerComponent } from './components/parts/viewer/viewer.component';
-import { ListComponent } from './components/parts/list/list.component';
-import { ListItemComponent } from './components/parts/list-item/list-item.component';
 import { PageHeadComponent } from './components/parts/page-head/page-head.component';
 import { PageComponent } from './components/parts/page/page.component';
 import { DashboardComponent } from './components/parts/dashboard/dashboard.component';
 import { CategoriesComponent } from './components/parts/categories/categories.component';
 import { PopularComponent } from './components/parts/popular/popular.component';
+import { InfoCardDirective } from './directives/info-card/info-card.directive';
+import { ListDirective } from './directives/list/list.directive';
+import { ListItemDirective } from './directives/list-item/list-item.directive';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, NavComponent, FootComponent, MainComponent, EditorComponent, ViewerComponent, ListComponent, ListItemComponent, PageHeadComponent, PageComponent, DashboardComponent, CategoriesComponent, PopularComponent ],
+  declarations: [ AppComponent, HelloComponent, NavComponent, FootComponent, MainComponent, EditorComponent, ViewerComponent, PageHeadComponent, PageComponent, DashboardComponent, CategoriesComponent, PopularComponent, InfoCardDirective, ListDirective, ListItemDirective ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/directives/list/list.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appList]'
+})
+export class ListDirective {
+
+  constructor() { }
+
+}
